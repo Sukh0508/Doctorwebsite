80526b2e8ac66530f9531614e5de8a87be59262b
Sukhwinder singh <sukhwinder86879@gmail.com>
2026-02-15 18:40:59 +0530

    chnages in nav and image

--- Doctorwebsite/script.js
+++ Doctorwebsite/script.js
@@ -1,14 +1,24 @@
 let closee = document.querySelector(".nav-links i");
-let hamburger=  document.querySelector(".navbar li");
+let hamburger = document.querySelector(".navbar li");
 let navlink = document.querySelector(".nav-links");
+let linkjump = document.querySelectorAll(".nav-links a");
 
-hamburger.addEventListener('click',function (e) {
+hamburger.addEventListener('click', function () {
    navlink.style.display = "flex";
+});
 
-    
-})
+closee.addEventListener('click', function () {
+   navlink.style.display = "none";
+});
 
-closee.addEventListener('click',function () {
-navlink.style.display = "none";
-    
-})
+linkjump.forEach(function (e){
+   e.addEventListener('click',function () {
+      if (window.innerWidth<=768) {
+         
+         navlink.style.display = "none";
+      }
+      
+   })
+   
+});
+
